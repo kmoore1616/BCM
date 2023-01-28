30c0d561de220b3d4eb9a8f6b7e6a6966b2a89e1
kmoore1616 <116331941+kmoore1616@users.noreply.github.com>
2023-01-28 15:22:56 -0700

    BCM 0.1
    This is the first working iteration of the program. It logs accelerometer and barometric/temperature changes, as well as time, to an SD card. In the future a more accurate clock may be helpful, but for now the delay clock works fine. Will update as more components arrive.

--- BCM.ino
+++ BCM.ino
@@ -2,26 +2,38 @@
 #include <millisDelay.h>
 #include <SPI.h>
 #include <SD.h>
-#include <DallasTemperature.h>
-#include <OneWire.h>
+#include "DHT.h"
+#include <Wire.h>
+#include "SparkFun_MMA8452Q.h"
 
 // Pins
 const int chipSelect = 53;
-
+#define DHTPIN 8
+#define DHTTYPE DHT22
 
 // Library Instantiation 
 File myFile;
 millisDelay clock;
+MMA8452Q accel;
+DHT dht(DHTPIN, DHTTYPE);
 
 // Variables
-int timeElapsed = 0;
+int seconds = 0;
+int minutes = 0;
+int hours = 0;
 
 void setup() {
   // Opens serial communications
   Serial.begin(9600);
 
   // Sets up sensor pins
-  sensors.begin();
+  dht.begin();
+  Wire.begin();
+
+  while(!accel.begin()){
+    Serial.println("Accelerometer not connected!");
+    return;
+  }
 
   // Debug stuff to make sure SD card works
   Serial.print("Initializing SD card...");
@@ -59,7 +71,7 @@ void setup() {
     Serial.print("Writing starter code...");
     // Writes Boilerplate Code
     myFile.println("****//BCM//****");
-    myFile.println("|Acc.|MainTherm|Humidity|AuxThem");
+    myFile.println("|Time|AccX,Y,Z|Them|Humidity|");
     myFile.close();
     Serial.println("  done");
   } else {
@@ -80,21 +92,84 @@ void setup() {
     myFile.close();
     Serial.println("done");
   } else {
-   if the file didn't open, print an error:
+   //if the file didn't open, print an error:
     Serial.println("error opening test.txt");
     return;
   }
 }
-
 void loop() {
   // Clock. Runs 5 seconds.
   if (!clock.isRunning()) {
     clock.start(5000);
   }
+  
   if (clock.justFinished()) {
     // Every 5 seconds runs in this. All sensor reading will occur here
-    timeElapsed += 5;
-    Serial.print("Time Elapsed: ");
-    Serial.println(timeElapsed);
+    seconds += 5;
+    if(seconds >= 60){
+      minutes += 1;
+      seconds = 0;
+    }
+    if(minutes >= 60){
+      hours += 1;
+      minutes = 0;
+    }
+    Serial.println("");
+    // Accelerometer Data Retrival
+    float x = accel.getCalculatedX();
+    float y = accel.getCalculatedY();
+    float z = accel.getCalculatedZ();
+
+    // Temp & Humidity Data Retrival
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature(true);
+
+    // Writing to disk - If you can concatante text please LMK cuz this is annoying.
+    myFile = SD.open("data.txt", FILE_WRITE);
+    if (myFile) {
+      myFile.print("| ");
+      myFile.print(hours);
+      myFile.print(".");
+      myFile.print(minutes);
+      myFile.print(".");
+      myFile.print(seconds);
+      myFile.print(" | ");
+      myFile.print(x);
+      myFile.print(", ");
+      myFile.print(y);
+      myFile.print(", ");
+      myFile.print(z);
+      myFile.print(" | ");
+      myFile.print(temperature);
+      myFile.print(" | ");
+      myFile.print(humidity);
+      myFile.println(" |");
+      myFile.close();
+      read();
+    }else{
+      // if the file didn't open, print an error:
+      Serial.println("error opening data.txt");
+      return;
+    }
+    
+  }
+}
+
+void read(){
+  // open the file for reading. This is only for debugging purposes:
+  myFile = SD.open("data.txt");
+  if (myFile) {
+    Serial.println("Reading...");
+  
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    myFile.close();
+    Serial.println("done");
+  } else {
+   //if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+    return;
   }
 }
